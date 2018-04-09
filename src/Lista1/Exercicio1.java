61916b6ef30d8686e487ef10f5b4324b1219805f
skynet <skynet@skynet>
2018-04-09 09:46:08 -0400

    Atualizei

---
+++ src/Lista1/Exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//Fa̧ca um algoritmo que receba quatro números, calcule e mostre a soma destes números.
+
+public class Exercicio1 {
+    public static void main(String [] args){
+        Scanner teclado = new Scanner(System.in);
+        int primeiroNumero, segundoNumero, terceiroNumero, quartoNumero;
+        System.out.println("Digite o Primeiro Numero");
+        primeiroNumero = teclado.nextInt();
+        System.out.println("Digite o Segundo Numero");
+        segundoNumero = teclado.nextInt();
+        System.out.println("Digite o Terceiro Numero");
+        terceiroNumero = teclado.nextInt();
+        System.out.println("Digite o Quarto Numero");
+        quartoNumero = teclado.nextInt();
+
+        int soma = primeiroNumero+segundoNumero+terceiroNumero+quartoNumero;
+
+        System.out.println("A soma desses quatro numeros é\n" +soma);
+
+    }
+}
