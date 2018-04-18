3003bbc364b012f9316705b7c76d39a6e85bc0f8
skynet <skynet@skynet>
2018-04-18 15:01:56 -0400

    Agora vai

---
+++ src/Variados/Hinos.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Variados;
+
+/**
+ *
+ * @author skynet
+ */
+public class Hinos {
+    public static void main(String[] args) {
+            String [] hinos = new String
+    }
+    
+}
