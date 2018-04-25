2706c1c6c14a010ed541efb0142c277217daeadd
skynet <skynet@skynet>
2018-04-25 10:46:24 -0400

    agora foi porra

--- src/Uri/Uri_1173.java
+++ src/Uri/Uri_1173.java
@@ -17,26 +17,24 @@ public class Uri_1173 {
         int [] N = new int [10];
         N[0] = teclado.nextInt();
         
-        if (N [0]<= 50) {
-            for (int i = 1; i < N.length; i++) {
+        for (int i = 1; i < 10; i++) {
             N[i] = N[i-1]*2;
             
         }
-            for (int i = 0; i < N.length; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.printf("N[%d] = %d\n", i, N[i]);
-            
+            }
         }
-            
-        
-            
-        }
-        else{
-            return;
-        }
-        
-            
-        }
-        
     }
+
+
+    
+
+
+       
+            
+        
+        
+    
     
 
