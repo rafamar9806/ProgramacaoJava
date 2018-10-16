ed5f793e80e0390c554d4ebe460c7efaf3958ef0
Rafael Silva Martins <201819070751@lab01>
2018-10-16 19:26:02 -0400

    ordena vetor com metodos

---
+++ src/vetores/OrdenaTest.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class OrdenaTest{
+	public static void main(String[] args) {
+		int [] vetOriginal = {7,8,12,988,4,8};
+
+		int [] vetcopia = new int[vetOriginal.length];
+
+		System.arraycopy(vetOriginal, 0, vetcopia, 0, vetOriginal.length);
+		Arrays.sort(vetcopia);
+
+		for (int i :vetOriginal ) {
+			System.out.print(i + " ");
+			
+		}
+		System.out.println();
+
+		for (int j :vetcopia ) {
+			System.out.print(j + " ");
+			
+		}
+		System.out.println();
+	}
+}
