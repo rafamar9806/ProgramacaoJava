a2504d22428a9f51168f8b2d05c08d0fc34b4dfa
rafamar9806 <rafa_silva.martins@hotmail.com>
2019-04-02 10:17:40 -0400

    exercicios uri, 1144 e 1145

--- Uri/Uri_1144.java
+++ Uri/Uri_1144.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
-public class Uri_1144 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class Uri_1144{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        int entrada = input.nextInt();
+		int entrada = input.nextInt();
 
-        for (int i = 1; i <= entrada; i++) {
-            for (int j = i; j < 2; j++) {
-
-                System.out.println(i + " " + ((i * j)));
-
-            }
-
-        }
-
-    }
+		for (int i = 1;i<=entrada ;i++ ) {
+			System.out.println(i+" "+(i*i)+" "+(i*i*i));
+			System.out.println(i+" "+(i*i+1)+" "+(i*i*i+1));				
+				
+			
+			
+		}
+	}
 }
---
+++ Uri/Uri_1145.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Uri_1145{
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int x = input.nextInt();
+		int y = input.nextInt();
+
+		for (int i = 1;i<=y ;i++ ) {
+			System.out.print(i+" ");
+				
+				
+			
+			
+		}
+	}
+}
